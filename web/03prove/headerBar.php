52b01ab75608c09bb76dd278b51a9b81624a4419
Nick Hendricks <hen17013@byui.edu>
2019-05-09 23:11:00 -0600

    css update2

--- web/03prove/headerBar.php
+++ web/03prove/headerBar.php
@@ -1,13 +1,17 @@
 <?php
   echo 
-  '<div class="banner"><img src="images/banner.png" alt="EthansToyCorner Banner"></div>
-    <div class="mContainer">
-      <div></div>
-        <div>
-          <a href="browse.php">Browse</a>
-          <a href="cart.php">Cart</a>
-          <a href="checkout.php">Checkout</a>
-        </div>
-      <div></div>
-    </div>';
+  '<div class="imgbox">
+     <div class="center-fit">    
+       <img src="images/banner.png" alt="EthansToyCorner Banner">
+     </div>
+   </div> 
+   <div class="mContainer">
+     <div></div>
+       <div>
+         <a href="browse.php">Browse</a>
+         <a href="cart.php">Cart</a>
+         <a href="checkout.php">Checkout</a>
+       </div>
+     <div></div>
+   </div>';
  ?>
