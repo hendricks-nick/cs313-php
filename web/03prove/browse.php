130ca4b2f50a522408b90cf5b8c7a5278411740a
Nick Hendricks <hen17013@byui.edu>
2019-05-09 23:02:16 -0600

    css update

--- web/03prove/browse.php
+++ web/03prove/browse.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>title</title>
+        <title>Ethan's Toy Corner</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="alt_style.css">
     </head>
     <body>
-	  <?php include 'headerBar.php';?>
+      <?php include 'headerBar.php';?>
+      <div class="bContainer">
+          <div>
+              
+          </div>
+      </div>
     </body>
 </html>
--- web/03prove/headerBar.php
+++ web/03prove/headerBar.php
@@ -4,9 +4,9 @@
     <div class="mContainer">
       <div></div>
         <div>
-          <a href="homepage.php">Browse</a>
-          <a href="about.php">Cart</a>
-          <a href="assignments.php">Checkout</a>
+          <a href="browse.php">Browse</a>
+          <a href="cart.php">Cart</a>
+          <a href="checkout.php">Checkout</a>
         </div>
       <div></div>
     </div>';
