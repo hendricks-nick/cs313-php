7bd16099979adf6b5a07ab306c802bb19c36509b
Nick Hendricks <hen17013@byui.edu>
2019-05-09 19:54:50 -0600

    CSS page update

--- web/03prove/browse.php
+++ web/03prove/browse.php
@@ -2,6 +2,8 @@
 <html>
     <head>
         <title>title</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="alt_style.css">
     </head>
     <body>
 	  <?php include 'headerBar.php';?>
---
+++ web/03prove/headerBar.php
@@ -0,0 +1,13 @@
+<?php
+  echo 
+  '<div class="banner"><img src="banner.png" alt="EthansToyCorner Banner"></div>
+    <div class="mContainer">
+      <div></div>
+        <div>
+          <a href="homepage.php">Browse</a>
+          <a href="about.php">Cart</a>
+          <a href="assignments.php">Checkout</a>
+        </div>
+      <div></div>
+    </div>';
+ ?>
