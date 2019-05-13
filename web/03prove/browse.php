df73ae00fc6097120bf6facda432ed666938c73c
Nick Hendricks <hen17013@byui.edu>
2019-05-12 21:56:22 -0600

    test

--- web/03prove/browse.php
+++ web/03prove/browse.php
@@ -21,37 +21,37 @@
           <img src="images/basketball.jpg" alt="basketball hoop">
           <br>
           <div class="prodTitle"> Basketball Hoops </div>
-          <button class="addCartBtn" type="button" onclick="addToCart(bbHoop)">Add To Cart</button>
+          <button class="addCartBtn" type="button" value="hoop" onclick="addToCart(this)">Add To Cart</button>
         </div>
         <div class="product">
           <img src="images/potato_head.jpg" alt="potato head">
           <br>
           <div class="prodTitle">Mr. Potato Head</div>
-          <button class="addCartBtn" type="button" onclick="addToCart('potato')">Add To Cart</button>
+          <button class="addCartBtn" type="button" value="potato" onclick="addToCart(this)">Add To Cart</button>
         </div>
         <div class="product">
           <img src="images/rubiks.jpg" alt="rubiks">
           <br>
           <div class="prodTitle">Rubiks Cube</div>
-          <button class="addCartBtn" type="button" onclick="addToCart(rubik)">Add To Cart</button>
+          <button class="addCartBtn" type="button" value="rubik" onclick="addToCart(this)">Add To Cart</button>
         </div>
         <div class="product">
           <img src="images/bloks.jpg" alt="bloks">
           <br>
           <div class="prodTitle">Building Bloks</div>
-          <button class="addCartBtn" type="button" onclick="addToCart(bloks)">Add To Cart</button>
+          <button class="addCartBtn" type="button" value="blok" onclick="addToCart(this)">Add To Cart</button>
         </div>
         <div class="product">
           <img src="images/trike.jpg" alt="tricycle">
           <br>
           <div class="prodTitle">Tricycle</div>
-          <button class="addCartBtn" type="button" onclick="addToCart(trike)">Add To Cart</button>
+          <button class="addCartBtn" type="button" value="trike" onclick="addToCart(this)">Add To Cart</button>
         </div>
         <div class="product">
           <img src="images/truck.jpg" alt="truck">
           <br>
           <div class="prodTitle">Toy Truck</div>
-          <button class="addCartBtn" type="button" onclick="addToCart(truck)">Add To Cart</button>
+          <button class="addCartBtn" type="button" value="truck" onclick="addToCart(this)">Add To Cart</button>
         </div>
       </div>
     </body>
--- web/03prove/cart.js
+++ web/03prove/cart.js
@@ -1,4 +1,5 @@
-function addToCart(item) {
+function addToCart(element) {
+    var item = element.value;
     $.post('/addcart.php', item, function(response) {
         // Log the response to the console
         console.log("Response: "+response);
