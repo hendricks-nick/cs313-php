a72f4b98f4c3c5b4502dd616ae7c8f179626a3cf
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2019-05-13 10:04:09 -0600

    test

--- web/03prove/cart.js
+++ web/03prove/cart.js
@@ -1,5 +1,14 @@
 function addToCart(element) {
     var item = element.id;
     console.log("Response: " + item);
+	$.ajax({
+    type: "POST",
+    url: "addcart.php",
+    data: item,
+        success: function(msg){
+           alert( "Data Saved: " + msg );
+               
+        }
+    });
     
   }
