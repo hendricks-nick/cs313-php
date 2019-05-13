3dbf12cdb1ee73e761185ecb958f312a64eb98fe
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2019-05-13 10:07:53 -0600

    test

--- web/03prove/browse.php
+++ web/03prove/browse.php
@@ -12,6 +12,7 @@
         <title>Ethan's Toy Corner</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="alt_style.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
         <script type="text/javascript" src="cart.js"></script>  
     </head>
     <body>
