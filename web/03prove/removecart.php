6f48ba8021efc0cfdf609acc44dca2d773f4ee7a
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2019-05-13 13:09:01 -0600

    fix formatting v4

--- web/03prove/removecart.php
+++ web/03prove/removecart.php
@@ -2,10 +2,17 @@
 	session_start();
 	$id = $_POST['item'];
 	
+	// Remove Cart Item
     unset($_SESSION['cartItems'][$id]);
 	
+	// Adjust the total
 	$_SESSION['cartTotal'] -= (int)$_SESSION['cartPrices'][$id];
 	
+	// remove the price
 	unset($_SESSION['cartPrices'][$id]);	
+	
+	// reseat the array index
+	array_values($_SESSION['cartItems']);
+	array_values($_SESSION['cartPrices']);
 	
 ?>
