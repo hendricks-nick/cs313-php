13cfeb81682ff5c078b75f2d7f489dec95d99322
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2019-05-13 10:39:41 -0600

    test

--- web/03prove/addcart.php
+++ web/03prove/addcart.php
@@ -1,10 +1,9 @@
 <?php
     session_start();
-	echo "console.log('addcart' " . $_POST['item'] .")";
     if ($_POST['item'] == 'potato') {
 		array_push($_SESSION['cartItems'], "Mr. Potato Head");
         array_push($_SESSION['cartPrices'], "25.00");
         $_SESSION['cartTotal'] += 25.00;
-		
+		return "Success";
     }
 ?>
