37abdf964f105a8ef739f24f3d8e49f7551c23f5
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2019-05-13 12:36:44 -0600

    remove test

--- web/03prove/addcart.php
+++ web/03prove/addcart.php
@@ -6,4 +6,34 @@
         $_SESSION['cartTotal'] += 25.00;
 		return "Success";
     }
+	else if ($_POST['item'] == 'hoop') {
+		array_push($_SESSION['cartItems'], "Basketball Hoop");
+        array_push($_SESSION['cartPrices'], "15.00");
+        $_SESSION['cartTotal'] += 15.00;
+		return "Success";
+    }
+	else if ($_POST['item'] == 'rubik') {
+		array_push($_SESSION['cartItems'], "Rubiks Cude");
+        array_push($_SESSION['cartPrices'], "10.00");
+        $_SESSION['cartTotal'] += 10.00;
+		return "Success";
+    }
+	else if ($_POST['item'] == 'blok') {
+		array_push($_SESSION['cartItems'], "Building Bloks");
+        array_push($_SESSION['cartPrices'], "12.00");
+        $_SESSION['cartTotal'] += 12.00;
+		return "Success";
+    }
+	else if ($_POST['item'] == 'trike') {
+		array_push($_SESSION['cartItems'], "Tricycle");
+        array_push($_SESSION['cartPrices'], "40.00");
+        $_SESSION['cartTotal'] += 40.00;
+		return "Success";
+    }
+	else if ($_POST['item'] == 'truck') {
+		array_push($_SESSION['cartItems'], "Toy Truck");
+        array_push($_SESSION['cartPrices'], "18.00");
+        $_SESSION['cartTotal'] += 18.00;
+		return "Success";
+    }
 ?>
