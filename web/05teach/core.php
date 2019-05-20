5217a937537e140a648dcb3cc26299b371998d1d
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2019-05-20 12:00:41 -0600

    teach05 core reqs

---
+++ web/05teach/core.php
@@ -0,0 +1,46 @@
+<?php
+	try
+	{
+	  $dbUrl = getenv('DATABASE_URL');
+
+	  $dbOpts = parse_url($dbUrl);
+
+	  $dbHost = $dbOpts["host"];
+	  $dbPort = $dbOpts["port"];
+	  $dbUser = $dbOpts["user"];
+	  $dbPassword = $dbOpts["pass"];
+	  $dbName = ltrim($dbOpts["path"],'/');
+
+	  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+	  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $ex)
+	{
+	  echo 'Error!: ' . $ex->getMessage();
+	  die();
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	  <style>
+	    form {
+		  text-align: center;
+		}
+		.scripture {
+		  font-weight: bold;
+		}
+	  </style>
+	</head>
+	<body>
+	  <h1> Scripture Resources </h1>
+		<?php
+			foreach ($db->query('SELECT * FROM scriptures') as $row)
+			{
+			  echo '<br><strong">' . $row['book'] . ' ' . $row['chapter'] 
+			  . ':' . $row['verse'] . '</strong> - ' . $row['content'];
+			}
+		?>
+	</body>
+<html>
