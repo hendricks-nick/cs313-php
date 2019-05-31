137c98e9df3fc13635af4b93f6c5116a02426a4c
Nick Hendricks <hen17013@byui.edu>
2019-05-31 06:30:15 -0600

    header file and css

--- web/project/companies.php
+++ web/project/companies.php
@@ -10,6 +10,6 @@
 		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     </head>
     <body>
-      <?php include 'header.php';?>  
+      <?php include 'header.php';?>
     </body>
 </html>
---
+++ web/project/header.php
@@ -0,0 +1,17 @@
+<?php
+echo '<div class="hContainer">
+  <div></div>
+    <div class="headerLinks">
+      <a href="contacts.php">Contacts</a>
+      <a href="companies.php">Companies</a>
+      <a href="dashboard.php">Dashboard</a>
+    </div>
+    <div></div>
+    <div class="search-container">
+      <form action="/action_page.php">
+        <input class="searchBox" type="text" placeholder="Search.." name="search">
+      </form>
+    </div>
+  <div></div>
+</div>';
+?>
