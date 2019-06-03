03f4fb6a418a94671cf1250ba159cb27d7af524b
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2019-06-03 08:05:00 -0600

    page edits

--- web/project/dashboard.php
+++ web/project/dashboard.php
@@ -10,6 +10,33 @@
 		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     </head>
     <body>
-      <?php include 'header.php';?>  
+      <?php include 'header.php';?> 
+	  <div class="pageTitle">
+	    <div></div>
+		<div><h1> Dashboard </h1></div>
+		<div></div>
+	  </div>
+	  <div class="bContainer">
+	    <div></div>
+		<div class="dbColumns">
+		  <h2> New Contacts </h2>
+		  <?php
+		    echo '';
+		  ?>
+		</div>
+		<div class="dbColumns">
+		  <h2> Follow Ups </h2>
+		  <?php
+		    echo '';
+		  ?>
+		</div>
+		<div class="dbColumns">
+		  <h2> Quick Lists </h2>
+		  <?php
+		    echo '';
+		  ?>
+        </div>
+        <div></div>
+	  </div>
     </body>
 </html>
