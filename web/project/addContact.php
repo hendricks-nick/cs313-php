1b3bd67ec424f26542f790d8c635c84af442295f
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2019-06-04 09:47:03 -0600

    edit5

--- web/project/addContact.php
+++ web/project/addContact.php
@@ -6,22 +6,26 @@
 	$lname = $_POST['lname'];
 	$phone = $_POST['phone'];
 	$email = $_POST['email'];
-	
-	$insert = "INSERT INTO contact (first_name, last_name, phone, email)
-			   VALUES (\'" . $fname . "\', \'"  . $lname . "\', \'"  . $phone . 
-			   "\', \'"  . $email . "\')";
-			   	   
-	$db->query($insert);
-	
-	/*if ($db->query($insert) === TRUE) {
-	  echo "Successfully Added";
+	try {
+		$insert = 'INSERT INTO contact(first_name, last_name, phone, email) VALUES (:fname, :lname, :phone, :email)';
+		$statement = $db->prepare($insert);
+		
+		$statement->bindValue(':fname', $fname);
+		$statement->bindValue(':lname', $lname);
+		$statement->bindValue(':phone', $phone);
+		$statement->bindValue(':email', $email);
+		
+		$statement->execute();
+		
+		$newId = $db->lastInsertId("contact_id_seq");
 	}
-	else {
-	  echo "Error Adding";
-	}*/
-
+	catch (Exception $err)
+	{
+		echo "Error with DB. Details: $err";
+		die();
+	}
+	
+	header("Location: contact.php?contact_id=" . $newId);
+	
+	die();
 ?>
-<script>
-  console.log(<?php echo json_encode($_POST); ?>);
-  console.log(<?php $insert; ?>);
-</script>
