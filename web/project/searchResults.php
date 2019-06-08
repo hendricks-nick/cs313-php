cb38dbc18ff882466172dda44058fcefb8ecb504
Nick Hendricks <hendricksn@byui.edu>
2019-06-08 01:47:10 -0600

    search results all columns 4

--- web/project/searchResults.php
+++ web/project/searchResults.php
@@ -31,18 +31,7 @@
 		  <div><h2>Follow Up Date</h2></div>
 		  <div></div>
 		  <?php
-				foreach ($db->query('SELECT * FROM contact WHERE lower(first_name) LIKE lower(\'%' . $_GET["search"] . '%\') OR ' .
-														'lower(medium_name) LIKE lower(\'%' . $_GET["search"] . '%\') OR ' .
-														'lower(last_name) LIKE lower(\'%' . $_GET["search"] . '%\') OR ' .
-														'lower(email) LIKE lower(\'%' . $_GET["search"] . '%\') OR ' . 
-														'lower(phone) LIKE lower(\'%' . $_GET["search"] . '%\') OR ' . 
-														'lower(title) LIKE lower(\'%' . $_GET["search"] . '%\') OR ' . 
-														'lower(company) LIKE lower(\'%' . $_GET["search"] . '%\') OR ' . 
-														'lower(street) LIKE lower(\'%' . $_GET["search"] . '%\') OR ' . 
-														'lower(city) LIKE lower(\'%' . $_GET["search"] . '%\') OR ' . 
-														'lower(state) LIKE lower(\'%' . $_GET["search"] . '%\') OR ' . 
-														'lower(zip) LIKE lower(\'%' . $_GET["search"] . '%\') OR ' . 
-														'lower(country) LIKE lower(\'%' . $_GET["search"] . '%\')') as $row)
+				foreach ($db->query('SELECT * FROM contact WHERE lower(first_name) LIKE lower(\'%' . $_GET["search"] . '%\')') as $row)
 				{
 				  echo '<div></div>' .
 				       '<div>' . $row['id'] . '</div>' .
