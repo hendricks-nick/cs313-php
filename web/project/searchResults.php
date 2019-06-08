2b5693be6c181d0cc086000982b32ab8d786eb7b
Nick Hendricks <hendricksn@byui.edu>
2019-06-08 01:10:57 -0600

    search results case insensitive

--- web/project/searchResults.php
+++ web/project/searchResults.php
@@ -31,7 +31,7 @@
 		  <div><h2>Follow Up Date</h2></div>
 		  <div></div>
 		  <?php
-				foreach ($db->query('SELECT * FROM contact WHERE first_name =\'' . $_GET["search"] . '\'') as $row)
+				foreach ($db->query('SELECT * FROM contact WHERE lower(first_name) =lower(\'' . $_GET["search"] . '\')') as $row)
 				{
 				  echo '<div></div>' .
 				       '<div>' . $row['id'] . '</div>' .
